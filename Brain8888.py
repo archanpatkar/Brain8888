38fa97bc210d09e572b40d2dbec917f5f8529faf
Archan Patkar <archanpatkar@gmail.com>
2018-06-07 12:02:05 +0530

    Update

--- Brain8888.py
+++ Brain8888.py
@@ -29,62 +29,113 @@ class Brain8888:
                 tokens.append(Token("Loop-End",char));
         return tokens;
 
-    def parse(self,tokens):
-        ast = { "type":"Root" , "children":[] };
-        i = 0;
-        while i < len(tokens):
-            token = tokens[i];
-            if(token.type == "Loop-Start"):
-                node = {"type":"Loop" , "children":[]}
-                while token.type !=  "Loop-End":
-                    if(token.type == "Loop-Start"):
-                        node = self.parse(tokens[i+1:]);
-                    else:
-                        node.get("children").append({ "operation":token.type , "symbol": token.symbol });
-                        i += 1;
-                        token = tokens[i];
-                node["children"] = node.get("children")[1:-1];
-                ast.get("children").append(node);
-            else:
-                ast.get("children").append({ "operation":token.type , "symbol": token.symbol });
-            i += 1;
-        return ast;
+    # def parse(self,tokens):
+    #     ast = { "type":"Root" , "children":[] };
+    #     i = 0;
+    #     while i < len(tokens):
+    #         token = tokens[i];
+    #         # if(token.type == "Loop-Start"):
+    #         #     node = {"type":"Loop" , "children":[]}
+    #         #     while token.type !=  "Loop-End":
+    #         #         if(token.type == "Loop-Start"):
+    #         #             node = self.parse(tokens[i+1:]);
+    #         #         else:
+    #         #             node.get("children").append({ "operation":token.type , "symbol": token.symbol });
+    #         #             i += 1;
+    #         #             token = tokens[i];
+    #             # node["children"] = node.get("children")[1:-1];
+    #             # ast.get("children").append(node);
+    #         # else:
+    #         ast.get("children").append({ "operation":token.type , "symbol": token.symbol });
+    #         i += 1;
+    #     return ast;
 
-    def interpret(self,code):
-        self.counter = 0;
-        instructions = code.get("children");
-        while self.counter < len(instructions):
-            token = instructions[self.counter];
-            print(token);
-            if token["operation"] == "Forward" :
+    def parse2(self,tokens):
+        ip = 0;
+        main = [];
+        while ip < len(tokens):
+            tok = tokens[ip];
+            if(tok.type == "Loop-Start"):
+                loop = [];
+                while tok.type != "Loop-End" and ip < len(tokens):
+                    tok = tokens[ip];
+                    loop.append(tok);
+                    ip = ip + 1;
+                loop = loop[1:-1];
+                main.append(self.parse2(loop));
+            else:
+                main.append(tok);
+            ip = ip + 1;
+        return main;
+
+    def interpret2(self,code):
+        counter = 0;
+        while counter < len(code):
+            token = code[counter];
+            if isinstance(token,list) :
+                print(token)
+                while self.tape[self.current] != 0:
+                    print("In Loop");
+                    print(self.tape[self.current]);
+                    print(token)
+                    self.interpret2(token)
+            elif token.type == "Forward" :
                 self.current += 1;
                 self.tape.append(0);
-            elif token["operation"] == "Backward" :
-                self.current -= 1;
-            elif token["operation"] == "Increment" :
-                self.tape[self.current] += 1;
-            elif token["operation"] == "Decrement" :
+            elif token.type == "Backward" :
                 if self.current > 0 :
-                    self.tape[self.current] -= 1;
+                    self.current -= 1;
                 else :
                     raise BaseException("Tape Already At The Start!");
-            elif token["operation"] == "Print" :
+            elif token.type == "Increment" :
+                self.tape[self.current] += 1;
+            elif token.type == "Decrement" :
+                self.tape[self.current] -= 1;
+            elif token.type == "Print" :
                 num = self.tape[self.current];
                 if num >= 0:
                     print(chr(num), end='');
-            elif token["operation"] == "Read" :
+            elif token.type == "Read" :
                 self.tape[self.current] = ord(input()[0]);
-            elif token["operation"] == "Loop-Start" :
-                while self.tape[self.current] != 0 :
-                    loop = token.get("children");
-                    counter = 0;
-                    while top.type != "Loop-End" :
-                        counter += 1;
-                        top = loop[counter];
-                        self.interpret({ "operation":"Step" , "children": [top]});
-            elif token["operation"] == "Loop-End" :
-                raise BaseException("Unexpected ']'");
+            counter += 1;
             self.counter += 1;
+
+
+    # def interpret(self,code):
+    #     self.counter = 0;
+    #     instructions = code.get("children");
+    #     while self.counter < len(instructions):
+    #         token = instructions[self.counter];
+    #         print(token);
+    #         if token["operation"] == "Forward" :
+    #             self.current += 1;
+    #             self.tape.append(0);
+    #         elif token["operation"] == "Backward" :
+    #             self.current -= 1;
+    #         elif token["operation"] == "Increment" :
+    #             self.tape[self.current] += 1;
+    #         elif token["operation"] == "Decrement" :
+    #             if self.current > 0 :
+    #                 self.tape[self.current] -= 1;
+    #             else :
+    #                 raise BaseException("Tape Already At The Start!");
+    #         elif token["operation"] == "Print" :
+    #             num = self.tape[self.current];
+    #             if num >= 0:
+    #                 print(chr(num), end='');
+    #         elif token["operation"] == "Read" :
+    #             self.tape[self.current] = ord(input()[0]);
+    #         elif token["operation"] == "Loop-Start" :
+    #             while self.tape[self.current] != 0 :
+    #                 loop = token.get("children");
+    #                 counter = 0;
+    #                 while top.type != "Loop-End" :
+    #                     counter += 1;
+    #                     top = loop[counter];
+    #                     self.interpret({ "operation":"Step" , "children": [top]});
+    #         elif token["operation"] == "Loop-End" :
+    #             raise BaseException("Unexpected ']'");
+    #         self.counter += 1;
 
 
     # def interpret(self,code):
