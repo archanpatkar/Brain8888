1f87142293153a95e0a96f92e7e83c92a1988ae9
Archan Patkar <archanpatkar@gmail.com>
2018-06-07 17:45:05 +0530

    Updated

--- Brain8888.py
+++ Brain8888.py
@@ -83,128 +83,17 @@ class Brain8888:
         foreground(GREEN);
         read = input("> ");
         while read != "q" and read != "quit" and read != "exit" and read != "bye":
-            tokens = self.tokenize(read);
-            ast = self.parse(tokens);
-            self.eval(ast);
+            if(read == "current"):
+                print("Position of Tape Head = ",self.current)
+                print("Value of Current Cell = ",self.tape[self.current])
+            else:
+                tokens = self.tokenize(read);
+                ast = self.parse(tokens);
+                self.eval(ast);
             read = input("> ");
         foreground(BLUE);
+        print("");
         print("May We Meet Again 🖖");
         print("May the Force be with you ✋");
         foreground(WHITE);
         background(BLACK);
-    # def parse(self,tokens):
-    #     ast = { "type":"Root" , "children":[] };
-    #     i = 0;
-    #     while i < len(tokens):
-    #         token = tokens[i];
-    #         # if(token.type == "Loop-Start"):
-    #         #     node = {"type":"Loop" , "children":[]}
-    #         #     while token.type !=  "Loop-End":
-    #         #         if(token.type == "Loop-Start"):
-    #         #             node = self.parse(tokens[i+1:]);
-    #         #         else:
-    #         #             node.get("children").append({ "operation":token.type , "symbol": token.symbol });
-    #         #             i += 1;
-    #         #             token = tokens[i];
-    #             # node["children"] = node.get("children")[1:-1];
-    #             # ast.get("children").append(node);
-    #         # else:
-    #         ast.get("children").append({ "operation":token.type , "symbol": token.symbol });
-    #         i += 1;
-    #     return ast;
-
-    # def interpret(self,code):
-    #     self.program_counter = 0;
-    #     instructions = code.get("children");
-    #     while self.program_counter < len(instructions):
-    #         token = instructions[self.program_counter];
-    #         print(token);
-    #         if token["operation"] == "Forward" :
-    #             self.current += 1;
-    #             self.tape.append(0);
-    #         elif token["operation"] == "Backward" :
-    #             self.current -= 1;
-    #         elif token["operation"] == "Increment" :
-    #             self.tape[self.current] += 1;
-    #         elif token["operation"] == "Decrement" :
-    #             if self.current > 0 :
-    #                 self.tape[self.current] -= 1;
-    #             else :
-    #                 raise BaseException("Tape Already At The Start!");
-    #         elif token["operation"] == "Print" :
-    #             num = self.tape[self.current];
-    #             if num >= 0:
-    #                 print(chr(num), end='');
-    #         elif token["operation"] == "Read" :
-    #             self.tape[self.current] = ord(input()[0]);
-    #         elif token["operation"] == "Loop-Start" :
-    #             while self.tape[self.current] != 0 :
-    #                 loop = token.get("children");
-    #                 counter = 0;
-    #                 while top.type != "Loop-End" :
-    #                     counter += 1;
-    #                     top = loop[counter];
-    #                     self.interpret({ "operation":"Step" , "children": [top]});
-    #         elif token["operation"] == "Loop-End" :
-    #             raise BaseException("Unexpected ']'");
-    #         self.program_counter += 1;
-
-
-    # def interpret(self,code):
-    #     self.program_counter = 0;
-    #     while self.program_counter < len(code):
-    #         char = code[self.program_counter]
-    #         if(char == '['):
-    #             self.program_counter += 1;
-    #             while self.tape[self.current] != 0:
-    #                 self.iteration(code,self.program_counter)
-    #         else:
-    #             self.each(char);
-    #         self.program_counter += 1;
-    #
-    # def each(self,char):
-    #     if(char == '>'):
-    #         self.current += 1;
-    #         self.tape.append(0);
-    #     elif(char == '<'):
-    #         if(self.current > 0):
-    #             self.current -= 1;
-    #     elif(char == '+'):
-    #         self.tape[self.current] += 1;
-    #     elif(char == '-'):
-    #         self.tape[self.current] -= 1;
-    #     elif(char == '.'):
-    #         num = self.tape[self.current];
-    #         if num >= 0:
-    #             print(chr(self.tape[self.current]), end='');
-    #     elif(char == ','):
-    #         self.tape[self.current] = ord(input()[0]);
-    #
-    #
-    # def iteration(self,code,start):
-    #     char = code[self.program_counter];
-    #     while(char != ']'):
-    #         self.interpret(char);
-    #         self.program_counter += 1;
-    #         char = code[self.program_counter];
-    #     self.program_counter = start;
-
-    # def loop(self,code,start):
-    #     char = '';
-    #     end = 0;
-    #     while self.program_counter < len(code):
-    #         char = code[self.program_counter];
-    #         if(self.tape[self.current] == 0):
-    #             self.program_counter = end;
-    #             return;
-    #         if(char == '[' and self.tape[self.current] != 0):
-    #             self.program_counter += 1;
-    #             self.loop(code,self.program_counter);
-    #         if(char == ']' and self.tape[self.current] != 0):
-    #             end = self.program_counter;
-    #             self.program_counter = start;
-    #             char = code[self.program_counter];
-    #             self.each(char);
-    #         else:
-    #             self.each(char);
-    #         self.program_counter += 1;
