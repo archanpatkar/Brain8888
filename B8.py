f1d3b45de8f945b8eda97c06c5a4df433c859a55
archanpatkar <pat@archanpatkar.com>
2018-12-02 14:28:48 +0530

    Update

--- B8.py
+++ B8.py
@@ -7,32 +7,32 @@ Token = namedtuple('Token', 'type symbol')
 
 class Brain8888:
     def __init__(self):
-        self.tape = [0];
-        self.current = 0;
+        self.tape = [0]
+        self.current = 0
 
     def reset(self):
         self.__init__()
 
     def tokenize(self,code):
-        tokens = [];
+        tokens = []
         for char in code:
             if(char == ">"):
-                tokens.append(Token("Forward",char));
+                tokens.append(Token("Forward",char))
             elif(char == "<"):
-                tokens.append(Token("Backward",char));
+                tokens.append(Token("Backward",char))
             elif(char == "+"):
-                tokens.append(Token("Increment",char));
+                tokens.append(Token("Increment",char))
             elif(char == "-"):
-                tokens.append(Token("Decrement",char));
+                tokens.append(Token("Decrement",char))
             elif(char == "."):
-                tokens.append(Token("Print",char));
+                tokens.append(Token("Print",char))
             elif(char == ","):
-                tokens.append(Token("Read",char));
+                tokens.append(Token("Read",char))
             elif(char == "["):
-                tokens.append(Token("Loop-Start",char));
+                tokens.append(Token("Loop-Start",char))
             elif(char == "]"):
-                tokens.append(Token("Loop-End",char));
-        return tokens;
+                tokens.append(Token("Loop-End",char))
+        return tokens
 
     def parse(self,tokens):
         ip = 0
@@ -53,7 +53,7 @@ class Brain8888:
                 stack.append(tok)
         return stack
 
-    def get(self): return self.tape[self.current];
+    def get(self): return self.tape[self.current]
 
     def eval(self,code):
         program_counter = 0
@@ -61,57 +61,57 @@ class Brain8888:
             token = code[program_counter]
             if isinstance(token,list):
                 while self.get() != 0:
-                    self.eval(token);
+                    self.eval(token)
             elif token.type == "Forward" :
-                self.current += 1;
+                self.current += 1
                 if(self.current >= len(self.tape)):
-                    self.tape.append(0);
+                    self.tape.append(0)
             elif token.type == "Backward" :
                 if self.current > 0 :
-                    self.current -= 1;
+                    self.current -= 1
                 else :
-                    raise BaseException("Tape Already At The Start!");
+                    raise BaseException("Tape Already At The Start!") 
             elif token.type == "Increment" :
-                self.tape[self.current] += 1;
+                self.tape[self.current] += 1
             elif token.type == "Decrement" :
-                self.tape[self.current] -= 1;
+                self.tape[self.current] -= 1
             elif token.type == "Print" :
-                num = self.tape[self.current];
+                num = self.tape[self.current] 
                 if num >= 0 and num <= 256:
-                    print(chr(num), end='');
+                    print(chr(num), end='')
             elif token.type == "Read" :
-                self.tape[self.current] = ord(input());
-            program_counter += 1;
+                self.tape[self.current] = ord(input())
+            program_counter += 1
 
     def execute(self,code):
         self.eval(self.parse(self.tokenize(code)))
 
     def repl(self):
-        foreground(RED);
-        print("Brain8888 0.0.1");
-        print("Welcome Human 👋");
-        print("");
-        foreground(GREEN);
-        read = input("> ");
+        foreground(RED) 
+        print("Brain8888 0.0.1") 
+        print("Welcome Human 👋") 
+        print("") 
+        foreground(GREEN) 
+        read = input("> ") 
         while read != "q" and read != "quit" and read != "exit" and read != "bye":
             if(read == "current position"):
-                print("Position of Tape Head = ",self.current);
-                print("Value of Current Cell = ",self.tape[self.current]);
+                print("Position of Tape Head = ",self.current) 
+                print("Value of Current Cell = ",self.tape[self.current]) 
             elif(read == "tape"):
                 print(self.tape)
             elif(read == "reset"):
-                self.reset();
+                self.reset() 
             else:
-                tokens = self.tokenize(read);
-                ast = self.parse(tokens);
-                self.eval(ast);
-            read = input("> ");
-        foreground(BLUE);
-        print("");
-        print("May We Meet Again 🖖");
-        print("May the Force be with you ✋");
-        foreground(WHITE);
-        background(BLACK);
+                tokens = self.tokenize(read) 
+                ast = self.parse(tokens) 
+                self.eval(ast) 
+            read = input("> ") 
+        foreground(BLUE) 
+        print("") 
+        print("May We Meet Again 🖖") 
+        print("May the Force be with you ✋") 
+        foreground(WHITE) 
+        background(BLACK) 
 
 if __name__ == "__main__":
     interpreter = Brain8888()
