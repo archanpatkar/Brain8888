02ba446cc42a9a3aadb326ec64a2bf1394324dba
archanpatkar <pat@archanpatkar.com>
2018-12-04 08:58:15 +0530

    Update

--- Main.py
+++
@@ -1,4 +0,0 @@
-from Brain8888 import Brain8888
-LANG = Brain8888()
-LANG.repl()
-# LANG.eval(LANG.parse(LANG.tokenize("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")))
